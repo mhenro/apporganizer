9ff0c94c236049a19967257762396cb3504b6be2
mhenro <mhenro@gmail.com>
2018-04-06 22:47:09 +0300

    Deleted uneccessary files

--- src/main/java/org/mhenro/apporganizer/config/RootConfig.java
+++
@@ -1,9 +0,0 @@
-package org.mhenro.apporganizer.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Profile("production")
-@Configuration
-public class RootConfig {
-}
